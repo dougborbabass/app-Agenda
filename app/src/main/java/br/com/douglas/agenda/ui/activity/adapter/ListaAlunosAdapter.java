903e08dbb0678353924c0160e6d66eec7ae5f45b
dougborbabass <douglas.borba@acad.pucrs.br>
2021-10-23 20:57:09 -0300

    criado adapter personalizado com layout item alunos

---
+++ app/src/main/java/br/com/douglas/agenda/ui/activity/adapter/ListaAlunosAdapter.java
@@ -0,0 +1,69 @@
+package br.com.douglas.agenda.ui.activity.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.douglas.agenda.R;
+import br.com.douglas.agenda.model.Aluno;
+
+public class ListaAlunosAdapter extends BaseAdapter {
+
+    private final List<Aluno> alunos = new ArrayList<>();
+    private Context context;
+
+    public ListaAlunosAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return alunos.size();
+    }
+
+    @Override
+    public Aluno getItem(int pos) {
+        return alunos.get(pos);
+    }
+
+    @Override
+    public long getItemId(int pos) {
+        return alunos.get(pos).getId();
+    }
+
+    @SuppressLint("ViewHolder")
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View viewCriada = LayoutInflater.from(context)
+                .inflate(R.layout.item_aluno, viewGroup, false);
+
+        Aluno alunoDevolvido = alunos.get(i);
+
+        TextView nome = viewCriada.findViewById(R.id.item_aluno_nome);
+        nome.setText(alunoDevolvido.getNome());
+
+        TextView telefone = viewCriada.findViewById(R.id.item_aluno_telefone);
+        telefone.setText(alunoDevolvido.getTelefone());
+
+        return viewCriada;
+    }
+
+    public void clear() {
+        this.alunos.clear();
+    }
+
+    public void addAll(List<Aluno> alunos) {
+        this.alunos.addAll(alunos);
+    }
+
+    public void remove(Aluno aluno) {
+        this.alunos.remove(aluno);
+    }
+}
